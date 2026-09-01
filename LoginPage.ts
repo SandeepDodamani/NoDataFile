d942456715798d27b14eb9c82f96b7f3ae44bbe9
SandeepDodamani <115776689+SandeepDodamani@users.noreply.github.com>
2026-09-01 23:42:30 +0530

    Add files via upload

---
+++ LoginPage.ts
@@ -0,0 +1,34 @@
+import { expect, Locator, Page } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly usernameInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameInput = page.getByRole('textbox', { name: 'Username' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: 'Login' });
+  }
+
+  async gotoLoginPage() {
+    await this.page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login', {
+      waitUntil: 'domcontentloaded',
+    });
+    await expect(this.usernameInput).toBeVisible();
+    await expect(this.passwordInput).toBeVisible();
+    await expect(this.loginButton).toBeVisible();
+  }
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+
+  async loginAsAdmin() {
+    await this.login('Admin', 'admin123');
+  }
+}
